8f552dd99341bb7ee8835772923d13cbf200d859
alexanderpacherrez <pacherrezberru@gmail.com>
2024-09-22 00:18:48 -0500

    second program is running

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percent? ");
+        string percent = Console.ReadLine();
+        int number = int.Parse(percent);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+            Console.WriteLine($"Your note is {letter}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Good job. You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You will do good the next time.");
+        }
     }
 }
